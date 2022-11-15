e9d451b738eeb1f15c8898b6007110042ce6d465
myoostis <parkgin2011@naver.com>
2022-11-15 16:52:31 +0900

    04

--- main.c
+++ main.c
@@ -3,16 +3,22 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void swap(int *a, int *b){
-	int temp;
-	temp = *a;
-	*a = *b;
-	*b = temp;	
-}
-
-void main(void){
-	int a = 3;
-	int b = 5;
-	swap(&a, &b);
-	printf("a:%i, b:%i\n", a,b);
+int main(void){
+	int i;
+	int sum = 0;
+	int grade[5];
+	
+	for(i=0; i<5; i++){
+		printf("input grade[%i] = ", i);
+		scanf("%d", &grade[i]);
+	}
+	
+	for(i=0; i<5; i++){
+		printf("grade[%i] = %d\n", i, *(grade+i));
+		sum += *(grade+i);
+	}
+	
+	printf("avaerage : %i\n", sum/5);
+	
+	return 0;
 }
